fa6ab73a31f422778314eb7a893a4d0ca9f48ed6
Harsh <harsh10mahobia@gmail.com>
2025-03-29 22:49:02 +0530

    bug reported 02 : with /dist build, removed --cached dist

--- dist/middleware/error_handler.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-declare const errorHandler: (err: CustomError, req: Request, res: Response, next: NextFunction) => void;
-export default errorHandler;
